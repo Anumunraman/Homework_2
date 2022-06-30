f8e243cbdd0b7418503f1d63a914a6b91eea609e
nikolaev mikhail <6n2n69@gmail.com>
2022-06-30 20:16:07 +0300

    task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+string number = "54";
+
+if (number.Length > 2) Console.WriteLine(number[2]);
+else Console.WriteLine ("в числе менее трёх сиволов");
+*/
+
+int number = 27548554; //275%10 = 5
+while (number/1000 > 0)
+{
+    number/=10;
+    Console.WriteLine(number);
+}
+Console.WriteLine(number%10);
