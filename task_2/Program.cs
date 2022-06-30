9531c4ca0df9b4e2e153ee1f42966cb67b72a60a
nikolaev mikhail <6n2n69@gmail.com>
2022-06-30 20:32:12 +0300

    task 3

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -5,10 +5,11 @@ if (number.Length > 2) Console.WriteLine(number[2]);
 else Console.WriteLine ("в числе менее трёх сиволов");
 */
 
-int number = 27548554; //275%10 = 5
+Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+int number = Convert.ToInt32(num);
 while (number/1000 > 0)
 {
     number/=10;
-    Console.WriteLine(number);
 }
 Console.WriteLine(number%10);
---
+++ task_3/Program.cs
@@ -0,0 +1,45 @@
+﻿/* int number = -1;
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Это выходной день");
+
+}
+else if (number > 7 || number < 1)
+{
+    Console.WriteLine("Ввод неверный");
+}
+else Console.WriteLine("Будний день");
+*/
+Console.WriteLine("Введите день недели: ");
+string day = Console.ReadLine();
+int number = Convert.ToInt32(day);
+if (number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+
+else if (number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (number == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else if (number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else Console.WriteLine("Ввод невернй");
