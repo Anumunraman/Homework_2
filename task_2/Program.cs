eaaa55406f158f6c4211bed25f4a7a5fda7fcb21
nikolaev mikhail <6n2n69@gmail.com>
2022-07-02 00:21:19 +0300

    chenged task 2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,15 +1,16 @@
-﻿/*
-string number = "54";
+﻿
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
 
 if (number.Length > 2) Console.WriteLine(number[2]);
 else Console.WriteLine ("в числе менее трёх сиволов");
-*/
 
-Console.WriteLine("Введите число: ");
-string num = Console.ReadLine();
-int number = Convert.ToInt32(num);
+
+/* Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 while (number/1000 > 0)
 {
     number/=10;
 }
 Console.WriteLine(number%10);
+*/
